93f4a2253eee3c6207fd9f199f7b446e83d8c710
Jan Becvar <jan.becvar@gmail.com>
2012-11-20 02:56:43 +0100

    Fixes.

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -1,5 +1,11 @@
 class ElectionsController < ApplicationController
   def show
-    @election = Refinery::Elections::Election.find(params[:id])    
+    @election = Refinery::Elections::Election.find(params[:id])
+
+    @parties = Refinery::Parties::Party.joins(subject: :elections)
+      .where(refinery_elections: { id: @election.id }).order(:name);
+
+    @politicians = Refinery::Politicians::Politician.joins(subject: :elections)
+      .where(refinery_elections: { id: @election.id }).order(:firstname, :lastname);
   end
 end
