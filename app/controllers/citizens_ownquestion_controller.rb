bdcc30b0d2a0fa1421b64dd2b5b79d0b03d5ce11
otazky <milus.kotisova@volny.cz>
2012-12-10 16:11:34 +0100

    Update app/controllers/citizens_ownquestion_controller.rb

--- app/controllers/citizens_ownquestion_controller.rb
+++ app/controllers/citizens_ownquestion_controller.rb
@@ -2,14 +2,18 @@
 # 3 most popular of these questions may compete with the official candidates´ strategic questions
 
 class OwnQuestionController < ApplicationController 
+before_filter: get_ownquestion, :only => [:show, :destroy, :index]
 
-  def new
-		(params[:id])
+	def get_ownquestion
+	  @oq = OwnQuestion.find(params[:id])
 	end
 
+        def new
+
+	end
 
 	def create
-		@oq= OwnQuestion.create(:status => params[:status] )
+	  @oq= OwnQuestion.create(:status => params[:status] )
 	end
 
 	def index
@@ -17,14 +21,12 @@ class OwnQuestionController < ApplicationController
 	end
 
 	def show
-		@oq=OwnQuestion.find(params[:id])
 		render :action => 'status'
 	end 
 
 	def destroy
 		if session[:citizen_id] !=@ownquestion.citizen_id
 		flash[:notice] = "Opravdu chcete otázku smazat? Všechny dosud získané body od ostatních voličů se tím pádem vynulují."
-		@oq=OwnQuestion.find(params[:id])
 		@og.destroy
 	end
 	
