90bd201cee5f93f59a3bdaadd08792209bf2ad91
Petr Komín <info@pkis.cz>
2013-01-09 14:06:45 +0100

    Dámská padesátka

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -7,5 +7,9 @@ class ElectionsController < ApplicationController
 
     @politicians = Refinery::Politicians::Politician.joins(subject: :elections)
       .where(refinery_elections: { id: @election.id }).order(:lastname, :firstname)
+
+
+		@own_questions = Refinery::OwnQuestions::OwnQuestion.order("score desc, id") if @election.election_type_id==4
+		@own_questions = Refinery::OwnQuestions::OwnQuestion.where('d50').order("score desc, id") if @election.election_type_id==5
   end
 end
